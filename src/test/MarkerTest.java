b4f089b3659ccfd0aab23e81009eb886a7e60f53
TBarrett553 <barrett.teanna@gmail.com>
2020-06-30 16:34:56 +0000

    Initialized Cap Project with unfinished Marker class

--- src/test/MarkerTest.java
+++ src/test/MarkerTest.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** */
+/** This is for testing the capabilities of the Marker Class (not started)
 @RunWith(JUnit4.class)
 public final class MarkerTest {
   @Test
@@ -184,4 +184,5 @@ public final class MarkerTest {
     Assert.assertTrue(emptyMiddle.overlaps(range));
     Assert.assertTrue(emptyEnd.overlaps(range));
   }
-}
+  
+}*/
