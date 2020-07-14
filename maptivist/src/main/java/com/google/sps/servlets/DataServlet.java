ea2eb4c27b745360a0a2f964e0e6623039a8bb7e
TBarrett553 <barrett.teanna@gmail.com>
2020-07-14 17:47:48 +0000

    initialzed map, search box and marker functions

--- maptivist/src/main/java/com/google/sps/servlets/DataServlet.java
+++ maptivist/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -28,5 +28,6 @@ public class DataServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello world!</h1>");
+    response.sendRedirect("/index.html");
   }
 }
--- maptivist/src/main/webapp/script.js
+++ maptivist/src/main/webapp/script.js
@@ -17,21 +17,6 @@ and should be used for any map parameters or variables */
 var map;
 
 /**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-/**
 * Intializes the primary map for the main webpage
  */
 function initMap(){
@@ -54,47 +39,111 @@ function initMap(){
 
   // Adds the new marker to the map
   marker.setMap(map);
+
+  //Initializes the search box
+  searchBox();
 }
 
 /**
-* Adds a new Marker and info window to the map
+* Adds an Auto complete search box
+ */
+function searchBox(){
+  // Create the search box and info window results and link them to the UI element
+  var box = document.getElementById("search-container");
+  var input = document.getElementById("search-input");
+  var searchBox = new google.maps.places.SearchBox(input);
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById('infowindow-content');
+  infowindow.setContent(infowindowContent);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(box);
+  
+  var autocomplete = new google.maps.places.Autocomplete(input);
+
+  // Set the data fields to return when the user selects a place.
+  autocomplete.setFields(
+    ['address_components', 'geometry', 'icon', 'name']);
+
+  autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+        }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  
+    }
+
+    var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindowContent.children['place-icon'].src = place.icon;
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-address'].textContent = address;
+        infowindow.open(map);
+  }); 
+}
+/** Adds a new Marker based on form submission
  */
 
 function createMarker() {
-  map.addListener('click', function(mapsMouseEvent){
-  //var myLatlng = {lat: 44.8549, lng: -93.2422}
-    var marker = new google.maps.Marker({
-        position: mapsMouseEvent.latLng,
-        map: map,
-        title:document.getElementsById('marker-title')
-    });
-    
-    createInfowindow(marker);
+  var lat = parseFloat(document.getElementById('marker-lat').value);
+  var lng = parseFloat(document.getElementById('marker-lng').value);
 
-    // Adds the new marker to the map
-    marker.setMap(map);
+  var location = new google.maps.LatLng(lat, lng);
+  var title = document.getElementById('marker-title').textContent;
+
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title:title
   });
+
+  // Adds the new marker to the map  
+  marker.setMap(map);
+
+  map.panTo(marker.getPosition());
+
+  var infowindow = createInfowindow(marker.getPosition());
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  console.log(location);
 }
 
-function createInfowindow(marker) {
+/**
+* Adds an infowindow based on the marker creation form
+ */
+function createInfowindow(position) {
   //Set info window content from form
-  var title = document.getElementsById('marker-title');
-  var location = marker.position.toString();
-  var description = document.getElementsById('marker-description');
-  var link = document.getElementsById('marker-link');
-  var category = document.getElementsById('marker-category');
+  var title = document.getElementById('marker-title').textContent;
+  var location = position.toString();
+  var description = document.getElementById('marker-description').textContent;
+  var link = document.getElementById('marker-link').textContent;
+  var category = document.getElementsByClassName('marker-category').value;
   
   // Set the content of the info window. (A popup may need to be used instead)
-  var contentString = "<h1>" + title + "</h1>" + "<h2>" 
-  + location + "</h2>" + "<p>" + description + "</p>" + 
-  "<button>Upvote</button>" + "<button>Flag</button>" +
-  "<h3>" + category + "</h3>";
+  var contentString = `<div id="marker-window"><h1>${title}</h1><br><h2>${location}</h2><br><p>${description}</p><br><p>${link}</p><br>`
+  '<button>Upvote</button><br>' + '<button>Flag</button><br>' +
+  '<h3>' + category + '</h3></div>';
 
   var infowindow = new google.maps.InfoWindow({
     content: contentString
   });
 
-  infowindow.open(map, marker);
+  return infowindow;  
 }
 
 function openForm() {
