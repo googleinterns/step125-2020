64368f4655583b0e574903e898c7f162a6cda4e9
TBarrett553 <barrett.teanna@gmail.com>
2020-07-29 16:52:14 +0000

    Removed example marker and rebased master

--- maptivist/src/main/webapp/script.js
+++ maptivist/src/main/webapp/script.js
@@ -32,66 +32,6 @@ function initMap(){
     mapId: '837a93b1537b2a61'
   });
 
-  // EXAMPLE MARKER
-  var myLatlng = {lat: 44.8549, lng: -93.2422}
-
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    title: "Justice for George Floyd"
-  });
-
-  // Adds the new marker and infowindow to the map
-  marker.setMap(map);
-  var contentString =
-    `<div class="marker-window">
-    <h1>Justice for George Floyd</h1>
-    <br>
-    <h3>BLM</h3>
-    <br>
-    <h2>Mall of America</h2>
-    <br>
-    <p>This is an example marker from a past protest</p>
-    <br>
-    <a href="https://en.wikipedia.org/wiki/George_Floyd">Related source</a>
-    <br>
-    <div class="upvote">
-        <p>Counter: <span id="counter"></span></p>
-        <br>
-        <div class="upvote">
-            <button>Upvote</button>
-            <p>counter: </p>
-        </div>
-        <div class="flag">
-            <button onclick="flagFunction()">Flag</button>
-        </div>
-    <button type="submit" name="id" id="vote-button" value="e8fb4ec8-22c6-4128-8878-938fc3baf99d" onclick="postVote()">Upvote</button>       
-    </div>
-    <div class="flag">
-        <button>Flag</button>
-    </div>
-    </div>`;
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  google.maps.event.addListener(infowindow, 'domready', function() {
-    const id = document.getElementById("vote-button").value;
-    const params = new URLSearchParams();
-    params.append("id", id);
-    params.append("update", false);
-
-    fetch('/votes', {method: 'POST', body: params}).then(response => response.json()).then((vote) => {
-        document.getElementById("counter").innerHTML = vote;
-        console.log(vote);
-    });
-  });  
-
-  marker.addListener('click', () => {
-    infowindow.open(map, marker);
-  });
-
   // Initializes the search box
   searchBox();
   
@@ -226,6 +166,9 @@ function drawInfowindow(markerObj) {
   </div>
   </body>`;
 
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
   infowindow.domready = function() {
    var flag_btn =   document.getElementById("info_window_flag_button");
@@ -233,11 +176,8 @@ function drawInfowindow(markerObj) {
       // the closure captures the marker.
       flagFunction(marker);
    }
-}
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
+  }
+  
   google.maps.event.addListener(infowindow, 'domready', function() {
     const id = document.getElementById("vote-button").value;
     const params = new URLSearchParams();
